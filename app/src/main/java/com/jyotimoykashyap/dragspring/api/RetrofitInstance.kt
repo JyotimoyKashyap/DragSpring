26664ad3a96af394837650a4d4e5127a4a74a6bd
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2022-05-24 22:45:01 +0530

    Added dependencies

---
+++ app/src/main/java/com/jyotimoykashyap/dragspring/api/RetrofitInstance.kt
@@ -0,0 +1,12 @@
+package com.jyotimoykashyap.dragspring.api
+
+class RetrofitInstance {
+
+    companion object {
+        const val BASE_URL = "https://api.mocklets.com/p68348/"
+
+        private val retrofit by lazy {
+
+        }
+    }
+}
