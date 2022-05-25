91df7713110f57ca09c84a16d4d197dfd08ba470
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2022-05-25 19:47:26 +0530

    cleanup: redundant code clean up

--- app/src/main/java/com/jyotimoykashyap/dragspring/ui/DragViewModel.kt
+++ app/src/main/java/com/jyotimoykashyap/dragspring/ui/DragViewModel.kt
@@ -6,8 +6,6 @@ import android.os.*
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import android.view.animation.BounceInterpolator
-import android.view.animation.OvershootInterpolator
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.doOnLayout
 import androidx.dynamicanimation.animation.DynamicAnimation
